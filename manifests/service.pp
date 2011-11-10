4e13e5a324992581f3064cf188084544b8fa83e6
Brian L. Troutwine <brian@troutwine.us>
2011-11-10 15:56:34 -0500

    Require that sshd module manage it's own firewall rules.
    Previously CarePilot had central management of it's firewall rules,
    which was clearly not scalable. Now, each module is able to provide
    its own rules.
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,4 +1,8 @@
 class sshd::service {
+  iptables::resource::rule { ssh:
+    port => 22,
+    interface => 'eth0',
+  }
   service { $sshd::params::sshd_service_name:
     ensure => running,
     hasstatus => true,
